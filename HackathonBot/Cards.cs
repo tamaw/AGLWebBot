f9e678cc5a24492df9d7722fb12dc58a432feefb
Dylan George Field <DField@agl.com.au>
2017-07-14 09:36:20 +1000

    Added Giffy Service, phrases and card classes

---
+++ HackathonBot/Cards.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.Bot.Connector;
+
+namespace HackathonBot
+{
+    public static class Cards
+    {
+        public static Attachment GetHeroCard(string imageUrl, string link, string title, string subtitle)
+        {
+            var heroCard = new HeroCard
+            {
+                Title = title,
+                Subtitle = subtitle,
+                Images = new List<CardImage> { new CardImage(imageUrl) },
+                Buttons = new List<CardAction> { new CardAction(ActionTypes.OpenUrl, "View", value: link) }
+            };
+
+            return heroCard.ToAttachment();
+        }
+
+        public static Attachment GetInternetAttachment(string url, string filename = "image.jpg")
+        {
+            return new Attachment
+            {
+                Name = filename,
+                ContentType = "image/png",
+                ContentUrl = url
+            };
+        }
+
+        public static Attachment GetAnimationCard(string imageUrl)
+        {
+            var animationCard = new AnimationCard
+            {
+                Title = "Microsoft Bot Framework",
+                Subtitle = "Animation Card",
+                Image = new ThumbnailUrl
+                {
+                    Url = imageUrl
+                },
+                Media = new List<MediaUrl>
+        {
+            new MediaUrl()
+            {
+                Url = imageUrl
+            }
+        }
+            };
+
+            return animationCard.ToAttachment();
+        }
+    }
+}
--- HackathonBot/Controllers/MessagesController.cs
+++ HackathonBot/Controllers/MessagesController.cs
@@ -5,7 +5,7 @@ using System.Web.Http;
 using Microsoft.Bot.Builder.Dialogs;
 using Microsoft.Bot.Connector;
 
-namespace HackathonBot
+namespace HackathonBot.Controllers
 {
     [BotAuthentication]
     public class MessagesController : ApiController
